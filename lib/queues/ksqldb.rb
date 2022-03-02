51ca583dd709db8931b973172f10af2cd253a249
Lapo <lapoelisacci@gmail.com>
2022-03-02 08:22:59 +0100

    First commit

---
+++ lib/queues/ksqldb.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ksqldb/version"
+
+module Queues
+  module Ksqldb
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/queues/ksqldb/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Queues
+  module Ksqldb
+    VERSION = "0.1.0"
+  end
+end
